723b7bc46eb9fc89342f405cad63925789c09989
lia3air <58852345+lia3air@users.noreply.github.com>
2020-01-28 10:22:01 +0100

    Update add-secrets.sh
    #1

--- add-secrets.sh
+++ add-secrets.sh
@@ -3,5 +3,5 @@
 ARG1=$1
 CURRENT_DATE=$(date +'%Y-%m-%d %T')
 echo "Your 1st argument was: ${ARG1}"
-echo ${ARG1} ${CURRENT_DATE}  > secrets.txt
+echo ${ARG1} ${CURRENT_DATE}  >> secrets.txt
 chmod 700 secrets.txt
